12dbd8e50d1c13bec25a40c266de16e74e37d8db
ksun22 <96256972+ksun22@users.noreply.github.com>
2022-04-30 15:16:52 -0500

    Initial Commit

---
+++ heatwaves/heatwavesApp.swift
@@ -0,0 +1,17 @@
+//
+//  heatwavesApp.swift
+//  heatwaves
+//
+//  Created by Kevin Sun on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct heatwavesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
